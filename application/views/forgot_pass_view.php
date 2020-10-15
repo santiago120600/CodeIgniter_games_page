afb0d2a73f9087ef70d7371b9f60c04f361adcb2
santiago <santiagocastanonarvizu@gmail.com>
2020-10-14 22:28:31 -0500

    added forgot password view and register view

--- application/views/forgot_pass_view.php
+++ application/views/forgot_pass_view.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
+    <title>Forgot Password</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?=base_url("resources/assets/vendor/bootstrap/css/bootstrap.min.css");?>">
     <link href="<?=base_url("resources/assets/vendor/fonts/circular-std/style.css");?>" rel="stylesheet">
@@ -30,45 +30,32 @@
 
 <body>
     <!-- ============================================================== -->
-    <!-- login page  -->
+    <!-- forgot password  -->
     <!-- ============================================================== -->
     <div class="splash-container">
-      <!--Error Message Start-->
-        <?php if($this->session->flashdata('error_msg')){ ?>
-            <div class="alert alert-danger alert-mg-b" role="alert">
-                <?=@$this->session->flashdata('error_msg');?>
-            </div>
-        <?php } ?>
-        <!--Error Message End-->
-        <div class="card ">
-            <div class="card-header text-center"><span class="splash-description">Please enter your user information.</span></div>
+        <div class="card">
+            <div class="card-header text-center"><img class="logo-img" src="../assets/images/logo.png" alt="logo"><span class="splash-description">Please enter your user information.</span></div>
             <div class="card-body">
-                <?=form_open('login/auth', array('class'=>'user'));?>
+                <form>
+                    <p>Don't worry, we'll send you an email to reset your password.</p>
                     <div class="form-group">
-                        <input class="form-control form-control-lg" type="text" placeholder="Username" name="pEmail" id="username">
+                        <input class="form-control form-control-lg" type="email" name="email" required="" placeholder="Your Email" autocomplete="off">
                     </div>
-                    <div class="form-group">
-                        <input class="form-control form-control-lg" id="password" type="password" placeholder="Password" name="pPassword">
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
-                <?=form_close();?>
+                    <div class="form-group pt-1"><a class="btn btn-block btn-primary btn-xl" href="../index.html">Reset Password</a></div>
+                </form>
             </div>
-            <div class="card-footer bg-white p-0  ">
-                <div class="card-footer-item card-footer-item-bordered">
-                    <a href="<?=base_url("Register");?>" class="footer-link">Create An Account</a></div>
-                <div class="card-footer-item card-footer-item-bordered">
-                    <a href="<?=base_url("Forgot_pass");?>" class="footer-link">Forgot Password</a>
-                </div>
+            <div class="card-footer text-center">
+                <span>Don't have an account? <a href="<?=base_url("Register");?>">Sign Up</a></span>
             </div>
         </div>
     </div>
-  
     <!-- ============================================================== -->
-    <!-- end login page  -->
+    <!-- end forgot password  -->
     <!-- ============================================================== -->
     <!-- Optional JavaScript -->
     <script src="../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
     <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
 </body>
+
  
 </html>
