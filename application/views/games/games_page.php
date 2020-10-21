10cb122674ba079057b0283a5095c9334adfab98
santiago castañón <santiagocastanonarvizu@gmail.com>
2020-10-20 19:58:25 -0500

    fixed user privilege power to add categories and games, now admin is the only one who can

--- application/views/games/games_page.php
+++ application/views/games/games_page.php
@@ -1,16 +1,25 @@
+<?php
+  $current_session = $this->session->userdata('store_sess');
+?>
+
 <div class="container">
     <h1 class="h3 mb-4 text-gray-80">Games</h1>
     <div class="row">
         <div class="col-md-12 col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-12 col-lg-12">
-                        <div class="alert alert-secondary">
-                                <button class="btn btn-danger" data-toggle="modal" data-target="#modalView" id="openModal" data-opt="new"><i class="fa fa-plus"></i>  New Game</button>
+                    <?php if ($current_session->user_privilege == 'Administrator') {
+                        ?> 
+                            <div class="row">
+                                <div class="col-md-12 col-lg-12">
+                                <div class="alert alert-secondary">
+                                        <button class="btn btn-danger" data-toggle="modal" data-target="#modalView" id="openModal" data-opt="new"><i class="fa fa-plus"></i>  New Game</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        <?php
+                    } 
+                    ?>
                     <div class="row mg-t-15">
                         <div class="col-md-12 col-lg-12" id="data_container">
                             <?=$container_data;?>
